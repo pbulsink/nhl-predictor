fe491712d621a61107a381a7c12d03c217b01f86
Philip Bulsink <philip.bulsink@uottawa.ca>
2016-01-08 13:20:06 -0500

    Tidying of data cleaning script

--- r_scripts/nhl_data_cleaning.R
+++ r_scripts/nhl_data_cleaning.R
@@ -29,13 +29,13 @@ ot.fixer<-function(row) {
 
 nhl.data.prep<-function(df) {
   df<-df[,c('Date','Visitor','G','Home','G.1','X')]
-  df<-df[!is.na(df$G),]
+  colnames(df)<-c("Date", "AwayTeam","AG","HomeTeam","HG","OT.SO")
+  df<-df[!is.na(df$AG),]
   df$OT.Win<-apply(df,1,ot.tagger)
   scores<-do.call("rbind",apply(df,1,ot.fixer))
-  df$G<-scores$G
-  df$G.1<-scores$G.1
+  df$AG<-scores$AG
+  df$HG<-scores$HG
   df$Date<-as.Date(df$Date)
-  colnames(df)<-c("Date", "AwayTeam","AG","HomeTeam","HG","OT.SO","OT.Win")
   return(df)
 }
 
