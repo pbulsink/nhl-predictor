2a87515b3f8536ce90565dd8556ccb45383cf48c
Philip Bulsink <philip.bulsink@uottawa.ca>
2016-01-13 09:23:50 -0500

    Added seed to stabilize random values

--- r_scripts/render_all.R
+++ r_scripts/render_all.R
@@ -6,4 +6,5 @@ render_all<-function(){
     rmarkdown::render("./r_knits/03_dixon_coles_scores.Rmd")
     rmarkdown::render("./r_knits/04_simulating_a_season.Rmd")
     rmarkdown::render("./r_knits/05_evaluating_models.Rmd")
+    rmarkdown::render("./r_knits/06_alternate_to_dixon_coles_method.Rmd")
 }
